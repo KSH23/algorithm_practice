cb85ccbc90788f0160e3c133426527fe61fe53f2
kangsu02 <kangsu02@gmail.com>
2021-07-27 17:58:22 +0900

    second commit

--- SWEA/4835/4835.py
+++ SWEA/4835/4835.py
@@ -7,10 +7,14 @@ sys.stdin = open('4835_input.txt')
 
 
 def max_min_sum(m, num_list):
+    # 최댓값, 최솟값을 맨 첫번째 경우로 설정
     max_sum = min_sum = sum(num_list[0: m])
 
+    # 리스트를 한 칸씩 전진해 기준점을 잡음
     for main_idx in range(len(num_list) - m + 1):
         temp_sum = 0
+        
+        # 기준점에서 m칸의 숫자를 더한 후 최댓값 또는 최솟값을 갱신
         for idx in range(main_idx, main_idx + m):
             temp_sum += num_list[idx]
         if max_sum < temp_sum:
