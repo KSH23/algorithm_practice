67a2da4f4eac48d04b145ac124595a485242921b
kangsu02 <kangsu02@gmail.com>
2021-08-06 17:53:30 +0900

    second commit

--- BOJ/2605.py
+++ BOJ/2605.py
@@ -2,24 +2,21 @@
 
 
 def make_line(n, students):
-    result = []
+    result = []    # 줄을 반대로 세우고 저장할 리스트
     
     for i in range(len(students)):
         for j in range(0, i+1):
             if students[i] == j:
-                # print(result[0:j], [i+1], result[j:])
+                # 자신이 뽑은 숫자를 기준으로 result의 앞과 뒤를 자르고 그 사이로 들어감
                 result = result[0:j] + [i+1] + result[j:]
-                # print(result)
+        
+    result.reverse()    # 다시 뒤집으면 최종 결과
     
-    # while len(result) != n:
-    #     result = result.remove(0)
-    ans = result.reverse()
-    # print(ans)
     return result
+
 
 N = int(input())
 
 my_students = list(map(int, input().split()))
-
 
 print(" ".join(map(str, make_line(N, my_students))))
